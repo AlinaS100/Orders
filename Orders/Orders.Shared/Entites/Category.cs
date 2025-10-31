5980f06b20b05292bd84843ff4e29a3d39cd697e
AlinaS100 <alina.alinuztza@yahoo.com>
2025-10-31 20:55:38 +0200

    Complete Generic Controller

---
+++ Orders/Orders.Shared/Entites/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entites;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    [Display(Name = "Categoría")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+    public string Name { get; set; } = null!;
+}
