0511c7536d746c528bec950dec2495703b2fc2b1
AlinaS100 <alina.alinuztza@yahoo.com>
2025-11-12 21:23:42 +0200

    Add pagination on countries, states and cities.

--- Orders/Orders.Backend/Orders.Backend/UnitsOfWork/Implementations/StatesUnitOfWork.cs
+++ Orders/Orders.Backend/Orders.Backend/UnitsOfWork/Implementations/StatesUnitOfWork.cs
@@ -2,6 +2,7 @@
 using Orders.Backend.Repositories.Interfaces;
 using Orders.Backend.UnitsOfWork.Implementations;
 using Orders.Backend.UnitsOfWork.Interfaces;
+using Orders.Shared.DTOs;
 using Orders.Shared.Entites;
 using Orders.Shared.Responses;
 
@@ -15,6 +16,10 @@ public class StatesUnitOfWork : GenericUnitOfWork<State>, IStatesUnitOfWork
     {
         _statesRepository = statesRepository;
     }
+
+    public override async Task<ActionResponse<IEnumerable<State>>> GetAsync(PaginationDTO pagination) => await _statesRepository.GetAsync(pagination);
+
+    public override async Task<ActionResponse<int>> GetTotalRecordsAsync(PaginationDTO pagination) => await _statesRepository.GetTotalRecordsAsync(pagination);
 
     public override async Task<ActionResponse<State>> GetAsync(int id) => await _statesRepository.GetAsync(id);
 
